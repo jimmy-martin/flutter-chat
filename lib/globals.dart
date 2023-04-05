2af153da9c1ec4a6d5c6da279c2f8cfeede7d433
iharoun <islem.haroun@my-digital-school.org>
2023-04-05 16:21:53 +0200

    change => monUtilisateur to myUser

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat/models/user.dart';
 
-User monUtilisateur = User.empty();
+User myUser = User.empty();
 Color defaultColor = Colors.blue;
 String defaultImage = "Ajouter une image ici";
