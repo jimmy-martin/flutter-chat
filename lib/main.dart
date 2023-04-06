568f67ee189c571d2a0b0b357bf43f88c0806a0f
sofianeso95 <sofiane.chila@my-digital-school.org>
2023-04-06 13:49:32 +0200

    home page v1

--- lib/main.dart
+++ lib/main.dart
@@ -5,46 +5,261 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Flutter Home Page'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  TextEditingController mail = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController prenom = TextEditingController();
+  TextEditingController nom = TextEditingController();
+  List<bool> selection = [true, false];
+
+
+
+  //Méthode
+  popUp(){
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context){
+          if (defaultTargetPlatform == TargetPlatform.iOS){
+            return CupertinoAlertDialog(
+              title: const Text("Erreur"),
+              content: const Text("Votre email et/ou votre mot de passe sont incorrectes"),
+              actions: [
+                TextButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: const Text("ok")
+                )
+              ],
+
+            );
+          }
+          else
+          {
+            return AlertDialog(
+              title: const Text("Erreur"),
+              content: const Text("Votre email et/ou votre mot de passe sont incorrectes"),
+              actions: [
+                TextButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: const Text("ok")
+                )
+              ],
+            );
+          }
+        }
+    );
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Flutter Chat"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: bodyPage(),
+        ));
   }
+
+  Widget bodyPage() {
+    return Column(
+      children: [
+
+        ToggleButtons(
+
+          selectedColor: Colors.blue,
+
+          borderRadius: BorderRadius.circular(10),
+
+
+
+          onPressed: (int choix) {
+            if (choix == 0) {
+              setState(() {
+                selection[0] = true;
+                selection[1] = false;
+              });
+            } else {
+              setState(() {
+                selection[0] = false;
+                selection[1] = true;
+              });
+            }
+          },
+          isSelected: selection,
+          children: const [ Text("Connexion"),  Text("Inscription")       ],
+
+
+        ),
+
+        //image
+        const SizedBox(height:5),
+        Image.network("https://img.icons8.com/clouds/512/user.png"),
+        const SizedBox(height:5),
+
+
+
+
+
+        const SizedBox(height: 20),
+        const SizedBox(height: 20),
+        if (selection[0] == false)
+
+
+          Row(   children:[
+
+            Container( width: MediaQuery.of(context).size.width*0.5,
+              child: TextField(
+
+                controller: prenom,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: "Prénom"),
+              ),
+            ),
+
+
+            const SizedBox(width: 15),
+            if (selection[0] == false)
+
+              Container( width: MediaQuery.of(context).size.width*0.4,
+                child: TextField(
+
+                  controller: nom,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      hintText: "Nom"),
+                ),
+              ),
+          ]
+          ),
+
+
+
+
+
+        const SizedBox(height: 10),
+        TextField(
+          controller: mail,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              hintText: "Email"),
+
+        ),
+        const SizedBox(height: 10),
+        TextField(
+          controller: password,
+          obscureText: true,
+          decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              hintText: "Mot de passe"),
+        ),
+        const SizedBox(height: 10),
+
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            onPrimary: Colors.white,
+
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+
+          onPressed: (){
+
+            if(selection[0]== false){
+              //si on en mode inscription
+              FirestoreHelper().Inscription(mail.text, password.text, nom.text, prenom.text).then((value) {
+                //si la méthode fonctionne bien
+                setState(() {
+                  monUtilisateur = value;
+                });
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context){
+                      return DashBoardView(mail: mail.text, password: password.text);
+                    }
+                ));
+
+
+              }).catchError((onError){
+                //si on constate une erreur
+                popUp();
+
+              });
+            }
+            else
+            {
+              //si en mode connexion
+              FirestoreHelper().Connect(mail.text, password.text).then((value){
+                setState(() {
+                  monUtilisateur = value;
+                });
+                Navigator.push(context, MaterialPageRoute(
+                    builder: (context){
+                      return DashBoardView(mail: mail.text, password: password.text);
+                    }
+                ));
+              }).catchError((onError){
+                popUp();
+              });
+            }
+
+
+
+
+          },
+          child: const Text('Valider'),
+        )
+
+      ],
+    );
+  }
+}
+
+class Styles {
+  static final ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
+    primary: Colors.blue,
+    onPrimary: Colors.white,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    ),
+  );
 }
